19c195e75311e4dd5598666390959f90087098c1
juliana.oliveira <juliana.oliveira@tallos.com.br>
2023-03-14 20:13:03 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -1,19 +1,14 @@
 import { HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/https-helpers'
 // return any in http response cause error in try catch to return is undefined - type correct: HttpResponse in line 5
 export class SignUpController {
   handle (httpRequest: HttpRequest): any {
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name')
-      }
+      return badRequest(new MissingParamError('name'))
     }
     if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('email')
-      }
+      return badRequest(new MissingParamError('email'))
     }
   }
 }
