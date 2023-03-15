f8ec5230f87d410166e5f8adbb8696547b11acb1
juliana.oliveira <juliana.oliveira@tallos.com.br>
2023-03-15 00:37:38 -0300

    refactor: make SignUpcontroller conforms to a protocol

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -1,8 +1,8 @@
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/https-helpers'
 import { HttpRequest, HttpResponse } from '../protocols/http'
-// return any in http response cause error in try catch to return is undefined - type correct: HttpResponse in line 5
-export class SignUpController {
+import { Controller } from '../protocols/controller'
+export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
