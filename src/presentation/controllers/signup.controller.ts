75130e02f96de11f516be50f9c627fd091883bea
juliana.oliveira <juliana.oliveira@tallos.com.br>
2023-03-15 00:15:32 -0300

    feat: ensure SignupController return 400 if no passwordConfirmation is not provided

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -4,7 +4,7 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 // return any in http response cause error in try catch to return is undefined - type correct: HttpResponse in line 5
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password']
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -48,18 +48,18 @@ describe('Sigup Controller', () => {
   })
 })
 
-// describe('Sigup Controller', () => {
-//   test('Should return 400 if no email is provided', () => {
-//     const sut = new SignUpController()
-//     const httpRequest = {
-//       body: {
-//         name: 'any_name',
-//         password: 'any_pass',
-//         passwordConfirm: 'any_pass'
-//       }
-//     }
-//     const httpResponse = sut.handle(httpRequest)
-//     expect(httpResponse.statusCode).toBe(400)
-//     expect(httpResponse.body).toEqual(new MissingParamError('email'))
-//   })
-// })
+describe('Sigup Controller', () => {
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_pass'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+  })
+})
